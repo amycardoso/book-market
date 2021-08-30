aba809b4c96c24252b3bc47ca2db6961b20c2524
Amélia Alice <alice.cardoso10@gmail.com>
2021-08-29 21:05:26 -0300

    :tada: starting the project

---
+++ book-market/src/main/kotlin/com/amycardoso/bookmarket/BookMarketApplication.kt
@@ -0,0 +1,11 @@
+package com.amycardoso.bookmarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookMarketApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookMarketApplication>(*args)
+}
