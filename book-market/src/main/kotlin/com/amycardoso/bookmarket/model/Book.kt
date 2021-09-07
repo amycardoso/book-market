7d67544cc4e3d9794ad9aeb05b4188e23797e79a
Amélia Alice <alice.cardoso10@gmail.com>
2021-09-07 18:51:20 -0300

    :sparkles: blocking status update of canceled/deleted books

--- book-market/src/main/kotlin/com/amycardoso/bookmarket/model/Book.kt
+++ book-market/src/main/kotlin/com/amycardoso/bookmarket/model/Book.kt
@@ -11,10 +11,26 @@ class Book (
     var id: Int? = null,
     var name: String,
     var price: BigDecimal,
-    @Column
-    @Enumerated(EnumType.STRING)
-    var status: BookStatus? = null,
     @ManyToOne
     @JoinColumn(name = "customer_id")
     var customer: Customer? = null
-)
+) {
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = null
+        set(value) {
+            if(field == BookStatus.CANCELED || field == BookStatus.DELETED)
+                throw Exception("It's not possible to update a book with status $field")
+            field = value
+        }
+
+    constructor(id: Int? = null,
+                name: String,
+                price: BigDecimal,
+                customer: Customer? = null,
+                status: BookStatus?): this(id, name, price, customer) {
+        this.status = status
+    }
+
+}
