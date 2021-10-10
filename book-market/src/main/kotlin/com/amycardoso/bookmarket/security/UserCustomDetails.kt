097ed36e56e7a6f975e6c13b13abbef40a44cb4b
Amélia Alice <alice.cardoso10@gmail.com>
2021-10-10 12:26:03 -0300

    :construction: creating user details

---
+++ book-market/src/main/kotlin/com/amycardoso/bookmarket/security/UserCustomDetails.kt
@@ -0,0 +1,18 @@
+package com.amycardoso.bookmarket.security
+
+import com.amycardoso.bookmarket.enums.CustomerStatus
+import com.amycardoso.bookmarket.model.Customer
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class UserCustomDetails(val customer: Customer): UserDetails {
+    val id = customer.id
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = customer.roles.map { SimpleGrantedAuthority(it.description) }.toMutableList()
+    override fun getPassword(): String = customer.password
+    override fun getUsername(): String = customer.id.toString()
+    override fun isAccountNonExpired(): Boolean = true
+    override fun isAccountNonLocked(): Boolean = true
+    override fun isCredentialsNonExpired(): Boolean = true
+    override fun isEnabled(): Boolean = customer.status == CustomerStatus.ACTIVE
+}
