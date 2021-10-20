d2628e79fe4778f0ec3e7a7a671061a2f7ef6dad
Amélia Alice <alice.cardoso10@gmail.com>
2021-10-19 21:32:46 -0300

    :white_check_mark: purchase service tests

---
+++ book-market/src/test/kotlin/com/amycardoso/bookmarket/helper/ModelBuilderHelper.kt
@@ -0,0 +1,37 @@
+package com.amycardoso.bookmarket.helper
+
+import com.amycardoso.bookmarket.enums.CustomerStatus
+import com.amycardoso.bookmarket.enums.Role
+import com.amycardoso.bookmarket.model.Book
+import com.amycardoso.bookmarket.model.Customer
+import com.amycardoso.bookmarket.model.Purchase
+import java.math.BigDecimal
+import java.util.*
+
+fun buildCustomer(
+    id: Int? = null,
+    name: String = "customer name",
+    email: String = "${UUID.randomUUID()}@email.com",
+    password: String = "password"
+) = Customer (
+    id = id,
+    name = name,
+    email = email,
+    status = CustomerStatus.ACTIVE,
+    password = password,
+    roles = setOf(Role.CUSTOMER)
+)
+
+fun buildPurchase(
+    id: Int? = null,
+    customer: Customer = buildCustomer(),
+    books: MutableList<Book> = mutableListOf(),
+    nfe: String = UUID.randomUUID().toString(),
+    price: BigDecimal = BigDecimal.TEN
+) = Purchase (
+    id = id,
+    customer = customer,
+    books = books,
+    nfe = nfe,
+    price = price
+)
