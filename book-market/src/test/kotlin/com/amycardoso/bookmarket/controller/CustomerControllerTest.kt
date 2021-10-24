028418bd9fd9f2d541145ff972ef8483f679192f
Amélia Alice <alice.cardoso10@gmail.com>
2021-10-24 12:54:16 -0300

    :white_check_mark: adding more tests - customer controller

--- book-market/src/test/kotlin/com/amycardoso/bookmarket/controller/CustomerControllerTest.kt
+++ book-market/src/test/kotlin/com/amycardoso/bookmarket/controller/CustomerControllerTest.kt
@@ -1,20 +1,26 @@
 package com.amycardoso.bookmarket.controller
 
+import com.amycardoso.bookmarket.controller.request.PostCustomerRequest
 import com.amycardoso.bookmarket.helper.buildCustomer
 import com.amycardoso.bookmarket.repository.CustomerRepository
+import com.amycardoso.bookmarket.security.UserCustomDetails
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
-
+import kotlin.random.Random
 @SpringBootTest
 @AutoConfigureMockMvc
 @ContextConfiguration
@@ -27,6 +33,9 @@ class CustomerControllerTest {
 
     @Autowired
     private lateinit var customerRepository: CustomerRepository
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
 
     @BeforeEach
     fun setup() = customerRepository.deleteAll()
@@ -63,6 +72,40 @@ class CustomerControllerTest {
             .andExpect(jsonPath("$.items.[0].name").value(customer1.name))
             .andExpect(jsonPath("$.items.[0].email").value(customer1.email))
             .andExpect(jsonPath("$.items.[0].status").value(customer1.status.name))
+    }
+
+    @Test
+    fun `should create customer`() {
+        val request = PostCustomerRequest("Carlos", "${Random.nextInt()}@fakeemail.com", "123456")
+        mockMvc.perform(post("/customers")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isCreated)
+
+        val customers = customerRepository.findAll().toList()
+        assertEquals(1, customers.size)
+        assertEquals(request.name, customers[0].name)
+        assertEquals(request.email, customers[0].email)
+    }
+
+    @Test
+    fun `should get user by id when user has the same id`() {
+        val customer = customerRepository.save(buildCustomer())
+
+        mockMvc.perform(get("/customers/${customer.id}").with(user(UserCustomDetails(customer))))
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.id").value(customer.id))
+            .andExpect(jsonPath("$.name").value(customer.name))
+            .andExpect(jsonPath("$.email").value(customer.email))
+            .andExpect(jsonPath("$.status").value(customer.status.name))
+    }
+
+    @Test
+    fun `should return forbidden when user has different id`() {
+        val customer = customerRepository.save(buildCustomer())
+
+        mockMvc.perform(get("/customers/0").with(user(UserCustomDetails(customer))))
+            .andExpect(status().isForbidden)
     }
 
 }
